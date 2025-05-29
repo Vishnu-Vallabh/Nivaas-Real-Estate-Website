c869345c5a88de9154863bf0bac1594fddd09140
Vishnu-Vallabh <vishnug.be26@uceou.edu>
2025-05-29 15:08:31 +0530

    Initial commit

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck />,
+    heading: "Best interest rates on the market",
+    detail:
+      "We offer the most competitive interest rates to help you achieve your dream home without breaking the bank.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Our services ensure price stability, protecting you from unexpected market fluctuations.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Get the best value for your investment with our unbeatable market prices and transparent deals.",
+  },
+];
+export default data;
